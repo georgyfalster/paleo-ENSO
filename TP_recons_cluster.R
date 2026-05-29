7ee9aed8a9f3b8b144145dfedb1ab7dfd1a0f0ad
georgyfalster <georgina.falster@gmail.com>
2026-05-29 17:02:28 +0930

    update script and add figs

--- TP_recons_cluster.R
+++ TP_recons_cluster.R
@@ -1,12 +1,12 @@
 ## ---------------------------------------------------------------------------
 ##
-## Script name: see below
+## Script name: hierarchical cluster analysis of different paleo-ENSO reconstructions
 ##
-## Purpose of script:
+## Purpose of script: attempt to replicate Fig. 3 of https://doi.org/10.1002/wcc.70036 
 ##
 ## Script author: Georgina Falster
 ##
-## Date create/updated: 2023-02-20
+## Date create/updated: 2026-05-29
 ##
 ## Email: georgina.falster@adelaide.edu.au
 ##
@@ -14,13 +14,15 @@
 
 ## ----------------------------------------------------------------------------
 ##
-## 
-## MP - multi-proxy = 4 or more different archive types
-## CoralMP = 3 or more proxy types from coral
-## target given as simply 'ENSO' if the reconstruction wasn't trained explicitly on an index
-## Author_Year_ProxyType_Target_TargetSource_ProxyDist_Nested_Method_TemporalAveraging_ProxySensitivity_ProxyPretreatment
-## nested here means proxy depth changes through time. So, this is applicable to DA
+## Reconstruction metadata distilled as follows:
+## ## Author_Year_ProxyType_Target_TargetSource_ProxyDist_Nested_Method_TemporalAveraging_ProxySensitivity_ProxyPretreatment
 ##
+## MP = multi-proxy = 4 or more different archive types
+## CoralMP = 3 or more proxy types from coral
+##
+## target given as simply 'ENSO' if the reconstruction wasn't trained explicitly on an index
+##
+## nested here means proxy depth changes through time. So, this is applicable to DA
 ## 
 ## ----------------------------------------------------------------------------
 
@@ -47,9 +49,6 @@ fpath = "TP_recons/"
 # =============================================================================
 # get all the data from the various repos
 # =============================================================================
-
-# STILL NEED - have contacted authors 
-# https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2023JD040491
 
 # -----------------------------------------------------------------------------
 # first deal with records from Zenodo
@@ -152,8 +151,8 @@ Freund_2019_CoralSrCorald18O_NinoColdTongue_HadISST_tropicalIndoPacific_Nested_P
   filter(Season == 12) %>%
   select(year = age_CE, val = NCT)
 
-# kinda dicey and minimally explained transformation in this paper - check with Freund for specifics.
-# but these shouldn't really be included as they are more or less just the same data as the NCT and NWP recons
+# this transformation is minimally explained in the paper but I think is accurate
+# Ideally these shouldn't  be included as they are more or less just the same data as the NCT and NWP recons
 solve_N3_N4 <- function(Nct, Nwp) {
   
   a <- 2/5
@@ -172,16 +171,18 @@ solve_N3_N4 <- function(Nct, Nwp) {
   return(data.frame(N3 = N3, N4 = N4))
 }
 
-temp <- solve_N3_N4(Freund_2019_CoralSrCorald18O_NinoWarmPool_HadISST_tropicalIndoPacific_Nested_PCR_seasonal_Temp_detrended$val,
-            Freund_2019_CoralSrCorald18O_NinoColdTongue_HadISST_tropicalIndoPacific_Nested_PCR_seasonal_Temp_detrended$val)
-
-temp$year <- Freund_2019_CoralSrCorald18O_NinoWarmPool_HadISST_tropicalIndoPacific_Nested_PCR_seasonal_Temp_detrended$year
-
-Freund_2019_CoralSrCorald18O_Nino3_HadISST_tropicalIndoPacific_Nested_PCR_seasonal_Temp_detrended <- data.frame(year = temp$year, val = temp$N3)
-
-Freund_2019_CoralSrCorald18O_Nino4_HadISST_tropicalIndoPacific_Nested_PCR_seasonal_Temp_detrended <- data.frame(year = temp$year, val = temp$N4)
-
-rm(temp)
+{
+  temp <- solve_N3_N4(Freund_2019_CoralSrCorald18O_NinoWarmPool_HadISST_tropicalIndoPacific_Nested_PCR_seasonal_Temp_detrended$val,
+                      Freund_2019_CoralSrCorald18O_NinoColdTongue_HadISST_tropicalIndoPacific_Nested_PCR_seasonal_Temp_detrended$val)
+  
+  temp$year <- Freund_2019_CoralSrCorald18O_NinoWarmPool_HadISST_tropicalIndoPacific_Nested_PCR_seasonal_Temp_detrended$year
+  
+  Freund_2019_CoralSrCorald18O_Nino3_HadISST_tropicalIndoPacific_Nested_PCR_seasonal_Temp_detrended <- data.frame(year = temp$year, val = temp$N3)
+  
+  Freund_2019_CoralSrCorald18O_Nino4_HadISST_tropicalIndoPacific_Nested_PCR_seasonal_Temp_detrended <- data.frame(year = temp$year, val = temp$N4)
+  
+  rm(temp)
+}
 
 # https://www.ncei.noaa.gov/access/paleo-search/study/11194
 Li_2011_TreeWidth_ENSO_NA_NorthAmerica_Nested_PCA_JunJulAug_Moist_PDSIReconHighPassFilter <- read.table(url("https://www.ncei.noaa.gov/pub/data/paleo/treering/reconstructions/enso-li2011-noaa.txt"),header = TRUE) %>%
@@ -258,10 +259,27 @@ Torbenson_2019_TreeWidth_MEI_HadCRUT_NorthAmerica_Nested_PCA_NovToFeb_MoistTemp_
   select(year = age_calCE, val = eMEIall)
 
 # =============================================================================
-# get some metadata
+# Liu et al 2024
 # =============================================================================
 
-#Liu 2017 not included in Freund analysis
+# https://agupubs.onlinelibrary.wiley.com/doi/full/10.1029/2023JD040491
+
+# -----------------------------------------------------------------------------
+# This is the sole reconstruction that is not publicly available.
+# to run the code from here you will need to contact the authors 
+# to request the data. Or see at the base of this script for a version
+# that does not include the two reconstructions from this paper.
+# -----------------------------------------------------------------------------
+
+Liu_2024_Treed18OSpeld18O_Nino34_HadISST_Global_Nested_PCR_calyr_MoistTemp_nineYrHighPass <- read.csv(paste0(fpath, "Liu2024JGRA-ENSO-reconstruction.csv"), skip = 5) %>%
+  select(year, val = PCR.reconstruction)
+
+Liu_2024_Treed18OSpeld18O_Nino34_HadISST_Global_Nested_PCA_calyr_MoistTemp_nineYrHighPass <- read.csv(paste0(fpath, "Liu2024JGRA-ENSO-reconstruction.csv"), skip = 5) %>%
+  select(year, val = DCC.reconstruction)
+
+# =============================================================================
+# get some metadata
+# =============================================================================
 
 recon_names <- c("Braganza_2009_TreeWidthIce_ENSO_NA_PacificBasin_NonNested_PCA_calyr_MoistTemp_threeYrLowPass",
                  "Braganza_2009_TreeWidthIceCoralMP_ENSO_NA_PacificBasin_NonNested_PCA_calyr_MoistTemp_threeYrLowPass",
@@ -277,6 +295,8 @@ recon_names <- c("Braganza_2009_TreeWidthIce_ENSO_NA_PacificBasin_NonNested_PCA_
                  "Freund_2019_CoralSrCorald18O_Nino4_HadISST_tropicalIndoPacific_Nested_PCR_seasonal_Temp_detrended",
                  "Li_2011_TreeWidth_ENSO_NA_NorthAmerica_Nested_PCA_JunJulAug_Moist_PDSIReconHighPassFilter",
                  "Li_2013_TreeWidth_Nino34_Kaplan_CircumPacific_Nested_PCA_NovDecJan_Moist_DroughtAtlases",
+                 "Liu_2024_Treed18OSpeld18O_Nino34_HadISST_Global_Nested_PCR_calyr_MoistTemp_nineYrHighPass",
+                 "Liu_2024_Treed18OSpeld18O_Nino34_HadISST_Global_Nested_PCA_calyr_MoistTemp_nineYrHighPass",
                  "Stahle_1993_TreeWidth_SOI_NA_SouthernNorthAmerica_Nested_LinReg_DecJanFeb_Moist_InstrumentalPersistence",
                  "Stahle_1998_TreeWidth_SOI_NA_CircumPacific_Nested_LinReg_DecJanFeb_Moist_InstrumentalPersistence",
                  "Cook_2008_TreeWidth_Nino12_NA_SWUSMexico_Nested_PCR_DecJanFeb_Moist_InstrumentalPersistence",
@@ -428,14 +448,167 @@ ggplot() +
     aes(x = x, y = y, xend = xend, yend = yend, colour = col),linewidth = 1) +
   geom_text(data = ggd_aligned$labels,aes(x = x, y = y-label_offset, label = label),
     size = 3.5,angle = 0,hjust = 0,vjust = 0.5) +
-  labs(title = "ENSO & PWC reconstructions aligned") +
+  labs(title = "Dendrogram summarising hierarchical relationships between different reconstructions of the\nEl Niño Southern Oscillation or Pacific Walker Circulation",
+       subtitle = paste0("Calculated across ", min_year, "–", max_year, " CE")) +
   coord_flip(clip = "off") +
   scale_y_reverse() +
   scale_colour_identity() +
+  # add author details
+  annotate("text", x = -Inf, y = -Inf,
+           label = paste0("Created by: Georgy Falster, ", format(Sys.Date(), "%d %b %Y")),
+           hjust = -0.5, vjust = 01, size = 3, colour = "grey60") +
   theme_void() +
-  theme(plot.margin = margin(t = 5, r = 250, b = 5, l = 5),
+  theme(plot.margin = margin(t = 5, r = 250, b = 10, l = 5),
         axis.text = element_blank(),
         axis.title = element_blank())
 
-# it makes no difference whether the 'transformed' Freund recons are included or not
-# they just tack on that end branch with the NCT and NWP recons
+ggsave(paste0("tropicalPacific_reconstructions_", min_year, "–", max_year, "hclust_dendrogram_includingLiu2024.png"),
+       bg = "white", height = 17, width = 25, units = "cm")
+
+# Inclusion of the 'transformed' Freund recons does not affect the dendrogram.
+# They just tack on that end branch with the NCT and NWP recons
+
+# =============================================================================
+# Now a version only including publicaly available datasets 
+# =============================================================================
+
+# assumes you have run the line above which creates the 'recon_names' object
+
+recons_pub <- recon_names[-which(grepl("Liu_2024", recon_names))]
+
+# ---------------------
+# get the details
+# ---------------------
+
+reconPub_df <- matrix(NA, length(recons_pub), 15) %>%
+  set_colnames(c("Author","Year","ProxyType","Target","TargetSource","ProxyDist",
+                 "Nested","Method","TemporalAveraging","ProxySensitivity",
+                 "ProxyPretreatment","start", "end", "length", "res"))
+
+for(i in 1:(length(recons_pub))) {
+  
+  this_recon <- get(recons_pub[i])
+  
+  reconPub_df[i, 1] <- strsplit(recons_pub[i], "_")[[1]][1]
+  reconPub_df[i, 2] <- strsplit(recons_pub[i], "_")[[1]][2]
+  reconPub_df[i, 3] <- strsplit(recons_pub[i], "_")[[1]][3]
+  reconPub_df[i, 4] <- strsplit(recons_pub[i], "_")[[1]][4]
+  reconPub_df[i, 5] <- strsplit(recons_pub[i], "_")[[1]][5]
+  reconPub_df[i, 6] <- strsplit(recons_pub[i], "_")[[1]][6]
+  reconPub_df[i, 7] <- strsplit(recons_pub[i], "_")[[1]][7]
+  reconPub_df[i, 8] <- strsplit(recons_pub[i], "_")[[1]][8]
+  reconPub_df[i, 9] <- strsplit(recons_pub[i], "_")[[1]][9]
+  reconPub_df[i, 10] <- strsplit(recons_pub[i], "_")[[1]][10]
+  reconPub_df[i, 11] <- strsplit(recons_pub[i], "_")[[1]][11]
+  reconPub_df[i, 12] <- min(this_recon$year)
+  reconPub_df[i, 13] <- max(this_recon$year)
+  reconPub_df[i, 14] <- length(na.omit((this_recon$val)))
+  reconPub_df[i, 15] <- (max(this_recon$year)-min(this_recon$year))/length(na.omit((this_recon$val)))
+  
+  rm(this_recon)
+  
+}
+
+reconPub_df <- as.data.frame(reconPub_df) %>%
+  mutate_at(c("start", "end", "length", "res"), as.numeric) %>%
+  mutate(recon_num = 1:n()) %>%
+  mutate(recon_name = paste(recon_num, Author, Target, Method, ProxyType, sep = "_")) %>%
+  # mutate(recon_name = paste(recon_num, ProxyType, Target, Method, ProxyDist, sep = "_")) %>%
+  as_tibble()
+
+# -----------------------------------------------------------------------------
+# all the other steps
+# -----------------------------------------------------------------------------
+
+ref_ts <- filter(EmileGeay_2013_MP_Nino34_ERSST_GlobalTropics_Nested_RegEM_calyr_MoistTemp_raw,
+                 year >= 1900 & year <= 1920)$val
+
+all_reconsPub <- matrix(NA, length(min_year:max_year), length(recons_pub)) %>%
+  set_colnames(reconPub_df$recon_name)
+
+for(i in 1:(length(recons_pub))) {
+  
+  this_recon <- get(recons_pub[i])
+  
+  # first, align if necessary
+  check_ts <- filter(this_recon,
+                     year >= 1900 & year <= 1920)$val
+  
+  if(cor(ref_ts, check_ts) < 0) {
+    
+    this_recon$val <- this_recon$val*-1
+    print(paste0("flipped ", recons_pub[i]))
+  }
+  
+  this_recon <- filter(this_recon, year <= max_year & year >= min_year)
+  
+  all_reconsPub[ ,i] <- this_recon$val
+  
+  rm(this_recon)
+}
+
+
+recsPub_df <- as.data.frame(all_reconsPub) %>% mutate(year = min_year:max_year)
+
+recsPub_df_long <- recsPub_df %>% pivot_longer(-year, names_to = "recon", values_to = "val")
+
+# ---------------------
+# preprocessing
+# ---------------------
+
+datPub_scaled <- scale(all_reconsPub)
+
+# ---------------------
+# clustering
+# ---------------------
+
+{
+  dat_scaled_trans <- t(datPub_scaled)
+  d <- dist(dat_scaled_trans, method = "euclidean")
+  hc1p <- hclust(d, method = "average" )
+  hc1p[["labels"]] <- gsub("^[^_]*_", "", hc1p[["labels"]])
+}
+
+# ---------------------
+# convert to dendrograms
+# ---------------------
+
+n_branch = 7
+
+{
+  dgp_aligned <- as.dendrogram(hc1p)
+  
+  dgp_aligned <- color_branches(dgp_aligned, k = n_branch)
+  
+  ggdp_aligned <- as.ggdend(dgp_aligned)
+  
+}
+
+# ---------------------
+# the plot
+# ---------------------
+
+ggplot() +
+  geom_segment(data = mutate(ggdp_aligned$segments, col = ifelse(is.na(col), "black", col)),
+               aes(x = x, y = y, xend = xend, yend = yend, colour = col),linewidth = 1) +
+  geom_text(data = ggdp_aligned$labels,aes(x = x, y = y-label_offset, label = label),
+            size = 3.5,angle = 0,hjust = 0,vjust = 0.5) +
+  labs(title = "Dendrogram summarising hierarchical relationships between different reconstructions of the\nEl Niño Southern Oscillation or Pacific Walker Circulation",
+       subtitle = paste0("Calculated across ", min_year, "–", max_year, " CE, using only publicly available reconstructions")) +
+  coord_flip(clip = "off") +
+  scale_y_reverse() +
+  scale_colour_identity() +
+  # add author details
+  annotate("text", x = -Inf, y = -Inf,
+           label = paste0("Created by: Georgy Falster, ", format(Sys.Date(), "%d %b %Y")),
+           hjust = -0.5, vjust = 01, size = 3, colour = "grey60") +
+  theme_void() +
+  theme(plot.margin = margin(t = 5, r = 250, b = 10, l = 5),
+        axis.text = element_blank(),
+        axis.title = element_blank())
+
+ggsave(paste0("tropicalPacific_reconstructions_", min_year, "–", max_year, "hclust_dendrogram_excludingLiu2024.png"),
+       bg = "white", height = 17, width = 25, units = "cm")
+
+# the results are apparently quite stable! 
+# removing two unpublished timeseries otherwise does not alter the dendrogram
